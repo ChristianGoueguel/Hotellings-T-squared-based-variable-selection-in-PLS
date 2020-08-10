38675813907d8711dd61dda5239bf567c59fe7de
Christian L. Goueguel <59129468+ChristianGoueguel@users.noreply.github.com>
2020-08-10 06:35:34 -0400

    Update hotellingTsq_varSelec.R

--- hotellingTsq_varSelec.R
+++ hotellingTsq_varSelec.R
@@ -14,5 +14,15 @@ Tsq_pls <- plsVarSel::T2_pls(ytr = pls_format$Ca[1:n],
                              ncomp = ncomp, 
                              alpha = 0.01)
 
+# Selected variables (as ind.T2 in T2_pls function)
 X %<>% select(Tsq_pls$mv[[1]])
+
+# Running PLSR with the new matrix X
 source("plsr.R")
+plot_9 <- plot_4
+plot_10 <- plot_5
+plot_11 <- plot_6
+plot_12 <- plot_7
+plot_13 <- plot_8
+
+
